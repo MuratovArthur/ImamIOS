cad6279244b1fc0cf463d94ffc18e47f0bef24ae
nrakhay <nuralirakhay@gmail.com>
2023-08-04 22:11:46 +0600

    imam is typing localized

--- ImamAI/Elements/TypingAnimationView.swift
+++ ImamAI/Elements/TypingAnimationView.swift
@@ -2,12 +2,14 @@ import SwiftUI
 
 struct TypingAnimationView: View {
     @State private var dotCount = 1
+    @EnvironmentObject private var globalData: GlobalData
     private let maxDotCount = 3
     private let animationDuration = 0.5
     
+    
     var body: some View {
         HStack {
-            Text("Имам печатает" + String(repeating: ".", count: dotCount))
+            Text(NSLocalizedString("imam-is-typing", bundle: globalData.bundle ?? Bundle.main, comment: "chat screen") + String(repeating: ".", count: dotCount))
                 .animation(Animation.easeInOut(duration: animationDuration).repeatForever())
                 .onAppear {
                     startAnimation()
